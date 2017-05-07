1cc11b85476d3ae85e524d930b0fd43397d2686c
Natalie <nataliedeweerd@gmail.com>
2017-05-07 13:56:48 +0100

    Adding Bugs
    - JS to add browser and version to body added.
    - Bugs files added (listing what the bugs on the site are).
    - 3 bugs added.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 
-function main() {
+jQuery(function () {
+    'use strict';
 
-(function () {
-   'use strict';
-   
-  	$('a.page-scroll').click(function() {
+    $('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -17,7 +15,7 @@ function main() {
         }
       });
 
-	
+
     // Show Menu on Book
     $(window).bind('scroll', function() {
         var navHeight = $(window).height() - 600;
@@ -33,17 +31,32 @@ function main() {
         offset: 80
     });
 
-	// Hide nav on click
-  $(".navbar-nav li a").click(function (event) {
+    // Hide nav on click
+    $(".navbar-nav li a").click(function (event) {
     // check if window is small enough so dropdown is created
     var toggle = $(".navbar-toggle").is(":visible");
     if (toggle) {
       $(".navbar-collapse").collapse('hide');
     }
-  });
+    });
 
-}());
+    // Add browser specific classes to body
+    var browserInfo = getBrowserInfo();
+    $('body').addClass((browserInfo.browser).toLowerCase()).addClass((browserInfo.browser).toLowerCase()+'--'+(browserInfo.version).toLowerCase());
+}($));
 
-
-}
-main();
+function getBrowserInfo() {
+    var ua= navigator.userAgent, tem,
+    M= ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*([0-9|\.]+)/i) || [];
+    if(/trident/i.test(M[1])){
+        tem=  /\brv[ :]+(\d+)/g.exec(ua) || [];
+        return 'IE '+(tem[1] || '');
+    }
+    if(M[1]=== 'Chrome'){
+        tem= ua.match(/\b(OPR|Edge)\/(\d+)/);
+        if(tem!= null) return tem.slice(1).join(' ').replace('OPR', 'Opera');
+    }
+    M= M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
+    if((tem= ua.match(/version\/(\d+)/i))!= null) M.splice(1, 1, tem[1]);
+    return { 'browser': M[0], 'version': M[1] }
+};
